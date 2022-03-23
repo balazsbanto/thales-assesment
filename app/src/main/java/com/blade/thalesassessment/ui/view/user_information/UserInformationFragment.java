0e792f30a8c1536ba4467e3e9fb3fa7fb83ac82a
Balazs Banto <balazs.banto@onfido.com>
2022-03-23 15:25:08 +0100

    Added back navigation from the user information screen

--- app/src/main/java/com/blade/thalesassessment/ui/view/user_information/UserInformationFragment.java
+++ app/src/main/java/com/blade/thalesassessment/ui/view/user_information/UserInformationFragment.java
@@ -7,16 +7,20 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.blade.thalesassessment.R;
+import com.blade.thalesassessment.databinding.FragmentUserInformationBinding;
 
 public class UserInformationFragment extends Fragment {
 
     private UserInformationViewModel mViewModel;
+    private FragmentUserInformationBinding binding;
 
     public static UserInformationFragment newInstance() {
         return new UserInformationFragment();
@@ -25,10 +29,22 @@ public class UserInformationFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_user_information, container, false);
+        binding =  FragmentUserInformationBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.logoutButton.setOnClickListener(v -> {
+            NavHostFragment.findNavController(this)
+                    .navigate(R.id.action_userInformation_to_login);
+        });
+
+    }
+
+        @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(UserInformationViewModel.class);
